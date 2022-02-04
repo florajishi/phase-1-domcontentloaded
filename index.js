c662c7ce268aac7e2a0ac4dac0896a5e033bc145
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-02-04 17:17:51 -0600

    commit tree f6d7d974a62eeb0ed3bff1bc6ffe8c5575b6ec74

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded' , () =>{ //the complete function to execute any js that runs after HTML done loading
+    console.log("The DOM has loaded!")
+    let text = document.getElementById('text')
+    text.textContent = "This is really cool!"
+    console.log(text)
+})
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+)
